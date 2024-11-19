4270bc4ae9667ae6cd4f9a5a3ec98c922353a07e
FaithzaMandolang <s22210337@student.unklab.ac.id>
2024-11-20 03:20:38 +0800

    msh blm jdi

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,9 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
 import ProjectCard from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -33,7 +36,38 @@ const Projects = () => {
       []
     );
   });
-  const projects = [];
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
 
   return (
     <section className="project" id="projects">
